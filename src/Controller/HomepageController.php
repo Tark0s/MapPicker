9fffe7c726d6b2ed5e4076c41d77ffefabcc448f
Lukasz <tarkowski14@gmail.com>
2025-01-21 19:34:26 +0100

    Init map picker

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -10,10 +10,25 @@ class HomepageController extends AbstractController
     #[Route('/', name: 'homepage')]
     public function index(){
 
-        $randInt = random_int(1, 100);
+        $randInt = random_int(1,3);
+
+        $map = $this->pickMap($randInt);
 
         return $this->render('Homepage/index.html.twig', [
-            'number' => $randInt
+            'number' => $randInt,
+            'map' => $map
         ]);
+    }
+
+    private function pickMap(int $number): string
+    {
+        switch ($number) {
+            case 1:
+                return 'Mirage';
+            case 2:
+                return 'Nuke';
+            case 3:
+                return 'Ancient';
+        }
     }
 }
